6c4388bd1b3f914517c224309abc39a556f8e11f
Tahmid Hassan ridoy <75554934+TahmidhassanHridoy@users.noreply.github.com>
2023-03-23 12:27:53 +0600

    v1

---
+++ src/main/java/com/example/security/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.security.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demoController")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> Sayhello() {
+        return  ResponseEntity.ok("Hello from secured end point");
+    }
+
+}
